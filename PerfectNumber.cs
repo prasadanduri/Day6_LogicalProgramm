88fe23ffa8d8ea1bb66fb57cfd7cd8eb5fcfd68c
prasad <prasadanduri13gmail.com>
2022-04-09 16:29:14 +0530

    logical programming

---
+++ PerfectNumber.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgramming_06
+{
+    public class PerfectNumber
+    {
+        public static void CheckPerfectNumber()
+        {
+            int n, i, sum;
+            int max, min;
+
+            Console.WriteLine("Find the Perfect Number within the given range");
+            Console.WriteLine("Input the Starting rang or number");
+            min = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input the ending range or number");
+            max = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The Perfect number within the given range");
+
+            for (n = min; n < max; n++)
+            {
+                i = 1;
+                sum = 0;
+                while (i < n)
+                {
+                    if (n % i == 0)
+                        sum = sum + i;
+                    i++;
+                }
+                if (sum == n)
+                    Console.WriteLine("{0}", n);
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPorgramming_06
+
+{
+    public class Odd
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("find odd num ");
+            for (int i = 1; i < 20; i = i + 2)
+            {
+                flag = flag + 1;
+
+            }
+            Console.WriteLine("No of odd num are " + flag);
+
+        }
+    }
+}
